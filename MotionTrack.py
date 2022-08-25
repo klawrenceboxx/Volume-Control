662694ddee2babc5438fc70b099e4a4830703f9d
kincaid1207 <73080418+kincaid1207@users.noreply.github.com>
2022-08-25 00:33:05 -0400

    created the hand mesh

--- MotionTrack.py
+++ MotionTrack.py
@@ -5,12 +5,24 @@ import mediapipe as mp
 import time
 
 #The basic code needed to code any webcam
-#__________________________
-cap = cv2.VideoCapture()
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cv2.imshow("Image", img)
-    cv2.waitkey(1)
-#__________________________
+    key = cv2.waitKey(1)
+
+# code to let us escape the webcam
+    if key == 27:
+        break
